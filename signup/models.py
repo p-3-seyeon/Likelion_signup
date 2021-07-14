3edca50c31adbd685296eab94480d0c9d9b5caa3
p-3-seyeon <syp9908@likelion.org>
2021-07-14 19:48:37 +0900

    signup페이지 만들기

---
+++ signup/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+# from .models import Signup
+
+# Create your models here.
+class Signup(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    pub_date = models.DateTimeField()
+    email = models.EmailField(max_length=100)
+    introduce = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+    def info_summary(self):
+        return self.introduce[:100]
